7d3edfc7bb1761c13e4ffbf90d5baca764aaad81
Alexander Ruiz <AlexanderRuiz@Alexanders-MacBook-Air.local>
2018-07-02 00:01:18 +0100

    add files

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  
+  // set the background to a random color
+  var hue = 350;
+  
+  // cache the jquery elements to prevent dom queries during the animation events
+  var $body = $("body");
+  var $svg = $("svg");
+  var $word = $(".word");
+
+  // when the animation iterates
+  $("h1").on('click', function() {
+
+    // replace the header with a random word
+    var word = words[Math.floor(Math.random() * words.length)] + "!";
+    $word.text(word);
+
+    // update the background color
+    hue += 47;
+    $body.css("background-color", "hsl(" + hue + ", 100%, 50%)");
+  });
+});
+
+
+words = ["Alex","Fabio","Jorge","Joas","Jonathan","Vanessa"]
